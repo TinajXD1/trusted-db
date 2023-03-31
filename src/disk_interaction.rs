5b8f1814a334e0ee550470376519f5c9c8da55ad
TinajXD <112769690+TinajXD1@users.noreply.github.com>
2023-03-31 19:02:19 +0000

    database in folder

--- src/disk_interaction.rs
+++ src/disk_interaction.rs
@@ -1,22 +1,30 @@
-use std::fs::{File, OpenOptions};
+use std::fs::{File, OpenOptions, DirBuilder};
 use std::io::{prelude::*, BufReader};
 
-pub fn write_tdb(path: &str, data: &[u8]) -> std::io::Result<()> {
+pub fn create_tdb(path: &str) -> std::io::Result<()> {
+  let mut db = path;
+  
+  let mut builder = DirBuilder::new().recursive(true).create(path)?;
+  
+  Ok(())
+}
+
+pub fn write_column(path: &str, column: &str, data: &[u8]) -> std::io::Result<()> {
     let mut file_append = OpenOptions::new().write(true).append(false).open(path)?;
     file_append.write(data)?;
 
     Ok(())
 }
 
-pub fn append_tdb(path: &str, data: &[u8]) -> std::io::Result<()> {
+pub fn append_in_column(path: &str, column: &str, data: &[u8]) -> std::io::Result<()> {
     let mut file_append = OpenOptions::new().write(true).append(true).open(path)?;
     file_append.write(data)?;
 
     Ok(())
 }
 
-pub fn read_tdb(path: &str) -> std::io::Result<()> {
-    let file_read = File::open(path)?;
+pub fn read_column(path: &str, column: &str) -> std::io::Result<()> {
+    let file_read = File::open(path+column)?;
 
     let buf_reader = BufReader::new(file_read);
     for lines in buf_reader.lines() {
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod disk_interaction;
 
 fn main() {
     let message = "It doesn't work!".as_bytes();
-    let path = String::from("databases/test.tdb");
+    let path = String::from("databases/test");
 
-    
+
+    disk_interaction::create_tdb(&path);
     disk_interaction::write_tdb(&path, message);
     disk_interaction::append_tdb(&path, message);
     disk_interaction::read_tdb(&path);
