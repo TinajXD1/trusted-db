24f1f87f5404f5548e5ee71679263a9985e39f53
TinajXD <112769690+TinajXD1@users.noreply.github.com>
2023-03-31 19:29:22 +0000

    Working on files organisation(database/column.txt)

--- src/disk_interaction.rs
+++ src/disk_interaction.rs
@@ -1,32 +1,37 @@
-use std::fs::{File, OpenOptions, DirBuilder};
+use std::fs::{DirBuilder, File, OpenOptions};
 use std::io::{prelude::*, BufReader};
 
 pub fn create_tdb(path: &str) -> std::io::Result<()> {
-  let mut db = path;
-  
-  let mut builder = DirBuilder::new().recursive(true).create(path)?;
-  
-  Ok(())
+    let builder = DirBuilder::new().recursive(true).create(path)?;
+    assert!(std::fs::metadata(path).unwrap().is_dir());
+
+    Ok(())
 }
 
-pub fn write_column(path: &str, column: &str, data: &[u8]) -> std::io::Result<()> {
-    let mut file_append = OpenOptions::new().write(true).append(false).open(path)?;
-    file_append.write(data)?;
+pub fn create_column(path: &str, column: &str) -> std::io::Result<()> {
+    let path_to_column = String::from(path) + &String::from(column);
+    let mut file_append = OpenOptions::new().write(true).append(false).open(path_to_column)?;
+    file_append.write("Start".as_bytes())?;
 
     Ok(())
 }
 
 pub fn append_in_column(path: &str, column: &str, data: &[u8]) -> std::io::Result<()> {
-    let mut file_append = OpenOptions::new().write(true).append(true).open(path)?;
+    let path_to_column = String::from(path) + &String::from(column);
+
+    let mut file_append = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(path_to_column)?;
     file_append.write(data)?;
 
     Ok(())
 }
 
 pub fn read_column(path: &str, column: &str) -> std::io::Result<()> {
-    let file_read = File::open(path+column)?;
+    let path_to_column = String::from(path) + &String::from(column);
 
-    let buf_reader = BufReader::new(file_read);
+    let buf_reader = BufReader::new(path_to_column.as_bytes());
     for lines in buf_reader.lines() {
         println!("{}", lines?);
     }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ mod disk_interaction;
 fn main() {
     let message = "It doesn't work!".as_bytes();
     let path = String::from("databases/test");
-
+    let column = String::from("disk_interaction");
 
     disk_interaction::create_tdb(&path);
-    disk_interaction::write_tdb(&path, message);
-    disk_interaction::append_tdb(&path, message);
-    disk_interaction::read_tdb(&path);
+    disk_interaction::create_column(&path, &column);
+    disk_interaction::append_in_column(&path, &column, message);
+    disk_interaction::read_column(&path, &column);
 }
