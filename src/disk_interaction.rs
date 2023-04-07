31601173633df03fdd23f97b270100962d02069f
TinajXD <112769690+TinajXD1@users.noreply.github.com>
2023-04-07 18:17:11 +0000

    modified:   src/disk_interaction.rs

--- src/disk_interaction.rs
+++ src/disk_interaction.rs
@@ -2,23 +2,23 @@ use std::fs::{DirBuilder, File, OpenOptions};
 use std::io::{prelude::*, BufReader};
 
 pub fn create_tdb(path: &str) -> std::io::Result<()> {
-    let builder = DirBuilder::new().recursive(false).create(path)?;
+    let builder = DirBuilder::new().recursive(true).create(path)?;
     assert!(std::fs::metadata(path).unwrap().is_dir());
 
     Ok(())
 }
 
 pub fn create_column(path: &str, column: &str) -> std::io::Result<()> {
-    let path_to_column = String::from("") + path + "/" + column;
-    println!("Column: {}", path_to_column);
-    let mut file_append = OpenOptions::new().write(true).append(false).open(path_to_column)?;
-    file_append.write("Start".as_bytes())?;
+    let path_to_column = String::from("") + path + "/" + column + ".col";
+    println!("Column: {}", &path_to_column);
+    let mut file_create = File::create(&path)?;
+    file_create.write_all("Start".as_bytes())?;
 
     Ok(())
 }
 
 pub fn append_in_column(path: &str, column: &str, data: &[u8]) -> std::io::Result<()> {
-    let path_to_column = String::from("") + path + "/" + column;
+    let path_to_column = String::from("") + path + "/" + column + ".col";
 
     let mut file_append = OpenOptions::new()
         .write(true)
@@ -30,7 +30,7 @@ pub fn append_in_column(path: &str, column: &str, data: &[u8]) -> std::io::Resul
 }
 
 pub fn read_column(path: &str, column: &str) -> std::io::Result<()> {
-    let path_to_column = String::from("") + path + "/" + column;
+    let path_to_column = String::from("") + path + "/" + column + ".col";
 
     let buf_reader = BufReader::new(path_to_column.as_bytes());
     for lines in buf_reader.lines() {
