44defc0ff521e8f5189b621177101105d6a82cac
TinajXD <EgorSolomahin1@yandex.ru>
2023-04-15 22:25:55 +0500

    modified:   src/main.rs Попробую начать с хранения информации методом ключ - значение.
    I'll try to start with storing information using the key-value method.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-mod disk_interaction;
+//mod disk_interaction;
 
 fn main() {
     let message = "It doesn't work!".as_bytes();
-    let path = String::from("databases/test");
-    let column = String::from("disk_interaction");
+    //let path = String::from("databases/test");
+    //let column = String::from("disk_interaction");
 
-    disk_interaction::create_tdb(&path);
-    disk_interaction::create_column(&path, &column);
-    disk_interaction::append_in_column(&path, &column, message);
-    disk_interaction::read_column(&path, &column);
+    //disk_interaction::create_tdb(&path);
+    //disk_interaction::create_column(&path, &column);
+    //disk_interaction::append_in_column(&path, &column, message);
+    //disk_interaction::read_column(&path, &column);
 }
